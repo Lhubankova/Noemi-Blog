2febffb28dd3d7dc4098d3fd46804284e8be8b55
Lhubankova <ecgtiysqecgt[322>
2021-01-11 16:39:59 +0200

    add wow.js, fix scroll issue

--- js/main.js
+++ js/main.js
@@ -9,48 +9,4 @@ menuToggle.addEventListener('click', function () {
     body.classList.toggle('open');
 });
 
-//scroll reveal
-
-window.sr = ScrollReveal();
-
-sr.reveal('.animate-left-first', {
-    origin: 'left',
-    duration: 1000,
-    distance: '25rem',
-    delay: 2300
-});
-
-sr.reveal('.animate-right-first', {
-    origin: 'right',
-    duration: 1000,
-    distance: '25rem',
-    delay: 2300
-});
-
-sr.reveal('.animate-left', {
-    origin: 'left',
-    duration: 1000,
-    distance: '25rem',
-    delay: 300
-});
-
-sr.reveal('.animate-right', {
-    origin: 'right',
-    duration: 1000,
-    distance: '25rem',
-    delay: 300
-});
-
-sr.reveal('.animate-top', {
-    origin: 'top',
-    duration: 1000,
-    distance: '25rem',
-    delay: 1300
-});
-
-sr.reveal('.animate-bottom', {
-    origin: 'bottom',
-    duration: 1000,
-    distance: '25rem',
-    delay: 600
-});
+new WOW().init();
